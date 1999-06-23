5bf524791cae09a1f6fee63e980c654ddea89285
georgi <>
1999-06-23 11:05:27 +0000

    some little change

--- src/seenby.c
+++ src/seenby.c
@@ -2,16 +2,14 @@
 #include <common.h>    
 #include <fcommon.h>   
 #include <global.h>    
-//#include <seenby.h>
+#include <seenby.h>
 
-int seenbyComp ( s_ticfile *tic, s_addr Aka)
+int seenbyComp ( s_addr *seenby, int anzseenby, s_addr Aka)
 {
    int i;
 
-   for (i=0; i < tic->anzseenby; i++) {
-       if (addrComp (tic->seenby[i], Aka) == 0) {
-          return 0;
-       }
-   }
+   for (i=0; i < anzseenby; i++)
+      if (addrComp (seenby[i], Aka) == 0) return 0;
+
    return !0;
 }
