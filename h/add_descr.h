4d72ec852166903380c02c1e158b050a407a1174
georgi <>
1999-06-23 10:40:57 +0000

    add announce in file, remove desc in files.bbs

--- h/add_descr.h
+++ h/add_descr.h
@@ -1,6 +1,11 @@
-#ifndef ADD_DESCR_H_
-#define ADD_DESCR_H_
+#ifndef _ADD_DESCR_H_
+#define _ADD_DESCR_H_
 
-int add_description (char *descr_file_name, char *file_name, char *description);
+#include <fidoconfig.h>
+
+int add_description (char *descr_file_name, char *file_name, char **description, int count_desc);
+int removeDesc (char *descr_file_name, char *file_name);
+int announceInFile (char *announcefile, char *file_name, int size, char *area, s_addr origin, char **description, int count_desc);
+int announceNewFileecho (char *announcenewfileecho, char *c_area, char *hisaddr);
 
 #endif
