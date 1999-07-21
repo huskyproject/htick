1036d8890a91be8b3b4fea2686010f29b64ac110
georgi <>
1999-07-21 07:24:50 +0000

    int getDesc()

--- h/add_descr.h
+++ h/add_descr.h
@@ -1,11 +1,12 @@
 #ifndef _ADD_DESCR_H_
 #define _ADD_DESCR_H_
 
-#include <fidoconfig.h>
+#include <fcommon.h>
 
 int add_description (char *descr_file_name, char *file_name, char **description, int count_desc);
 int removeDesc (char *descr_file_name, char *file_name);
 int announceInFile (char *announcefile, char *file_name, int size, char *area, s_addr origin, char **description, int count_desc);
 int announceNewFileecho (char *announcenewfileecho, char *c_area, char *hisaddr);
+int getDesc (char *descr_file_name, char *file_name, s_ticfile *tic);
 
 #endif
