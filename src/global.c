6f74c8308d3d69eef062f5369aec5755065191a4
Max Chernogor <mche@users.sourceforge.net>
2002-06-26 05:21:07 +0000

    version changed

--- src/global.c
+++ src/global.c
@@ -46,12 +46,12 @@ char *print_ch(int len, char ch)
     return tmp;
 }
 
-const int VER_MAJOR   = 0;
-const int VER_MINOR   = 9;
-const int VER_PATCH   = 3;
+const int VER_MAJOR   = 1;
+const int VER_MINOR   = 0;
+const int VER_PATCH   = 0;
 
 /* branch is "" for CVS current, "-stable" for the release candiate branch  */
-const char *VER_BRANCH  = "";
+const char *VER_BRANCH  = "-stable";
 
 /* The service version string is empty for the first release done from a    */
 /* release candidate branch.  If subsequent service releases are necessary, */
