c656c197ac019ae579272d1cf6dfb7e9e3c9601b
Pavel Gulchouck <pgul@users.sourceforge.net>
2002-09-18 10:18:47 +0000

    Fix compiler warning

--- src/seenby.c
+++ src/seenby.c
@@ -24,6 +24,7 @@
  *****************************************************************************/
 
 #include <stdlib.h>
+#include <string.h>
 #include <fidoconf/fidoconf.h>
 #include <fidoconf/common.h>    
 #include <fcommon.h>   
