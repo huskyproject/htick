69f4aa7b7bf7d176ed7c47c265b913e1e1076824
Stas Degteff <stas_degteff@users.sourceforge.net>
2002-11-05 14:27:40 +0000

    Fix revision 1.23 and use simple  method

--- src/report.c
+++ src/report.c
@@ -433,9 +433,9 @@ void reportNewFiles()
     FILE      *rp;
     ps_anndef RepDef;
     char      *cp=NULL;
-#ifndef UNIX
+/*#ifndef UNIX
     char      *dp=NULL;
-#endif
+#endif*/
 
     for(i = 0; i < config->ADCount; i++)    
     {
@@ -481,30 +481,29 @@ void reportNewFiles()
             else
             {
                 /* if output to file:  \r ==> \r\n */
-                /*
                 cp=msg->text;
-                
-#ifdef UNIX
+/*#ifdef UNIX*/
                 while( (cp=strchr(cp, '\r')) )
                   *cp='\n';
-#else
+/*#else
                 for( j=0; (cp=strchr(cp, '\r')); j++ );
                 cp=msg->text;
-                dp = smalloc(strlen(msg->text)+1+j)
-                sstrcpy(msg->text, cp);
+                dp = smalloc(strlen(msg->text)+1+j);
                 for( dp=cp; *cp; dp++, cp++ )
                 { 
                     if( *cp=='\r' )
-                    { *(dp++)='\r'; *dp='\n' }
+                    { *(dp++)='\r'; *dp='\n'; }
                     else *dp = *cp;
                 }
                 *dp='\0';
                 nfree(msg->text);
                 msg->text = dp;
 #endif
-                */
+
                 msg->textLength = strlen(msg->text);
                 fwrite(msg->text, 1, msg->textLength, rp);
+*/
+                fprintf(rp, msg->text); /* Automatic translate "\n" to "\015\012" */
                 w_log(LL_FLAG, "Created report file: %s", msg->subjectLine);
                 fclose(rp);
             }
