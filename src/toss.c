008c938781f610706fdd7cd4348e622010ca3aab
Max Chernogor <mche@users.sourceforge.net>
2003-02-08 07:10:43 +0000

    fix compilation

--- src/toss.c
+++ src/toss.c
@@ -267,14 +267,16 @@ int parseTic(char *ticfile,s_ticfile *tic)
 #ifndef HAS_sopen
     tichandle=fopen(ticfile,"r");
 #else
-    int fh = 0;
-    /* insure that ticfile won't be removed while parsing */
-    fh = sopen( ticfile, O_RDWR | O_BINARY, SH_DENYWR);
-    if( fh<0 ){
-        w_log(LL_ERROR, "Can't open '%s': %s (sopen())", ticfile, strerror(errno));
-        return 0;
+    {
+        int fh = 0;
+        /* insure that ticfile won't be removed while parsing */
+        fh = sopen( ticfile, O_RDWR | O_BINARY, SH_DENYWR);
+        if( fh<0 ){
+            w_log(LL_ERROR, "Can't open '%s': %s (sopen())", ticfile, strerror(errno));
+            return 0;
+        }
+        tichandle = fdopen(fh,"r");
     }
-    tichandle = fdopen(fh,"r");
 #endif
     
     if(!tichandle){
