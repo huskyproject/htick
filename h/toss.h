fc749d278b90ebca0909c33bd4390ca237181a00
Stas Degteff <stas_degteff@users.sourceforge.net>
2011-02-26 22:11:56 +0000

    Add comment for each function

--- h/toss.h
+++ h/toss.h
@@ -35,17 +35,49 @@
 enum tossSecurity {secLocalInbound, secProtInbound, secInbound};
 typedef enum tossSecurity e_tossSecurity;
 
+/* Append tearline and origin lines into message text and create message into
+ * specified area.
+ * If areaName is NULL pointer or not exist: netmail will placed into first
+ * netmail area, echomail will placed into badarea.
+ */
 void writeMsgToSysop(s_message *msg, char *areaName, char* origin);
+
+/* Free memory allocated in elements of structure */
 void disposeTic(s_ticfile *tic);
+
+/* Write (create) TIC-file
+ * Return 0 if error, 1 if success
+ */
 int writeTic(char *ticfile,s_ticfile *tic);
+
+/* Scan directory with postponed files
+ */
 void checkTmpDir(void);
 void processTmpDir(void);
+
+/* Process TIC-files in all inbounds */
 void toss(void);
+
+/* Write netmail message into areaname or first netmail area (if areaname is NULL pointer) */
 void writeNetmail(s_message *msg, char *areaName);
+
+/*  Check lick access to fileecho for receiving files to link */
 int  readCheck(s_filearea *echo, s_link *link);
+/*  Check lick access to fileecho for receiving files from link */
 int  writeCheck(s_filearea *echo, ps_addr aka);
+
+/* Send a file "filename" to all subscribers of fileecho.
+ * Return: 0 if success, positive integer if error, negative integer if illegal call
+ */
 int  sendToLinks(int isToss, s_filearea *filearea, s_ticfile *tic, const char *filename);
+
+/* Read TIC file and store values into 2nd parameter.
+ * Return 1 if success, 0 if error.
+ * Clean 2nd parameter before processing if not error.
+ */
 int  parseTic(char *ticfile,s_ticfile *tic);
+
+/* Save ticket file into filearea-specific directory */
 void doSaveTic(char *ticfile,s_ticfile *tic, s_filearea *filearea);
 
 
